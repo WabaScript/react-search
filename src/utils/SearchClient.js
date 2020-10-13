ec976d17a758c76facd9e70263a23e807ef88e75
WabaScript <rjwahba@gmail.com>
2020-10-13 10:56:47 -0400

    remove

--- src/utils/SearchClient.js
+++ src/utils/SearchClient.js
@@ -1,5 +1,7 @@
 import algoliasearch from 'algoliasearch';
+const apiKey = process.env.REACT_APP_ALGOLIA_PUB_SEARCH_KEY
+const appID = process.env.REACT_APP_ALGOLIA_APP_ID
 
-const SearchClient = algoliasearch('DNHR48MVJQ', '002adb870e1771fb29baa60b60af2758');
+const SearchClient = algoliasearch(appID, apiKey);
 
 export default SearchClient;
